06c29cbad7b3775cdd09f92b057670d68107f933
Pablo Braulio <brulics@gmail.com>
2022-12-25 09:56:12 +0100

    Finish deploy

--- config/config.go
+++ config/config.go
@@ -7,17 +7,13 @@ import (
 
 const (
 	ProjectPrefix = "TELEGRAM_"
-	ServerURL     = ProjectPrefix + "SERVER_URL"
 	Token         = ProjectPrefix + "TOKEN"
-	AuthToken     = ProjectPrefix + "AUTH_TOKEN"
 	WSServerURl   = ProjectPrefix + "WS_URL"
 	WSServerToken = ProjectPrefix + "WS_TOKEN"
 )
 
 type Config struct {
-	serverUrl,
 	telegramToken,
-	authToken,
 	wsServerToken string
 	wsServerURL url.URL
 }
@@ -30,28 +26,12 @@ func (c Config) WsServerToken() string {
 	return c.wsServerToken
 }
 
-func (c Config) ServerUrl() string {
-	return c.serverUrl
-}
-
 func (c Config) TelegramToken() string {
 	return c.telegramToken
 }
 
-func (c Config) AuthToken() string {
-	return c.authToken
-}
-
 func NewConfig() (Config, error) {
-	serverURL, err := env.Value(ServerURL)
-	if err != nil {
-		return Config{}, err
-	}
 	token, err := env.Value(Token)
-	if err != nil {
-		return Config{}, err
-	}
-	auth, err := env.Value(AuthToken)
 	if err != nil {
 		return Config{}, err
 	}
@@ -68,9 +48,7 @@ func NewConfig() (Config, error) {
 		return Config{}, err
 	}
 	return Config{
-		serverUrl:     serverURL,
 		telegramToken: token,
-		authToken:     auth,
 		wsServerURL:   *wsServerURL,
 		wsServerToken: wsToken,
 	}, nil
